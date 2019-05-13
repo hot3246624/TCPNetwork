68cf79ac1f138a7d689931d9167b03e1a010b2e9
zhuliting <zhuliting@foxmail.com>
2019-05-13 16:11:52 +0800

    updated handle

--- x/nameservice/handler.go
+++ x/nameservice/handler.go
@@ -10,10 +10,12 @@ import (
 func NewHandler(keeper Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
 		switch msg := msg.(type) {
-		case MsgSetName:
-			return handleMsgSetName(ctx, keeper, msg)
-		case MsgBuyName:
-			return handleMsgBuyName(ctx, keeper, msg)
+		case MsgTransfer:
+			return handleMsgTransfer(ctx, keeper, msg)
+		case MsgContractDeploy:
+			return handleContractDeploy(ctx, keeper, msg)
+		case MsgContractExec:
+			return handleMsgContractExec(ctx, keeper, msg)
 		default:
 			errMsg := fmt.Sprintf("Unrecognized nameservice Msg type: %v", msg.Type())
 			return sdk.ErrUnknownRequest(errMsg).Result()
@@ -21,32 +23,24 @@ func NewHandler(keeper Keeper) sdk.Handler {
 	}
 }
 
-// Handle a message to set name
-func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) sdk.Result {
-	if !msg.Owner.Equals(keeper.GetOwner(ctx, msg.Name)) { // Checks if the the msg sender is the same as the current owner
+// Handle a message to transfer
+func handleMsgTransfer(ctx sdk.Context, keeper Keeper, msg MsgTransfer) sdk.Result {
+	if !msg.From.Equals(keeper.GetOwner(ctx, msg.From)) { // Checks if the the msg sender is the same as the current owner
 		return sdk.ErrUnauthorized("Incorrect Owner").Result() // If not, throw an error
 	}
-	keeper.SetName(ctx, msg.Name, msg.Value) // If so, set the name to the value specified in the msg.
+	// keeper.Transfer(ctx, msg.From, msg.To, msg.Value)
+
 	return sdk.Result{}                      // return
 }
 
-// Handle a message to buy name
-func handleMsgBuyName(ctx sdk.Context, keeper Keeper, msg MsgBuyName) sdk.Result {
-	if keeper.GetPrice(ctx, msg.Name).IsAllGT(msg.Bid) { // Checks if the the bid price is greater than the price paid by the current owner
-		return sdk.ErrInsufficientCoins("Bid not high enough").Result() // If not, throw an error
-	}
-	if keeper.HasOwner(ctx, msg.Name) {
-		_, err := keeper.coinKeeper.SendCoins(ctx, msg.Buyer, keeper.GetOwner(ctx, msg.Name), msg.Bid)
-		if err != nil {
-			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
-		}
-	} else {
-		_, _, err := keeper.coinKeeper.SubtractCoins(ctx, msg.Buyer, msg.Bid) // If so, deduct the Bid amount from the sender
-		if err != nil {
-			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
-		}
-	}
-	keeper.SetOwner(ctx, msg.Name, msg.Buyer)
-	keeper.SetPrice(ctx, msg.Name, msg.Bid)
+// Handle a message to deploy contract
+func handleContractDeploy(ctx sdk.Context, keeper Keeper, msg MsgContractDeploy) sdk.Result {
+	// TODO
+	return sdk.Result{}
+}
+
+// Handle a message to exec contract
+func handleMsgContractExec(ctx, ctx sdk.Context, keeper Keeper, msg MsgContractExec) sdk.Result {
+	// TODO
 	return sdk.Result{}
 }
