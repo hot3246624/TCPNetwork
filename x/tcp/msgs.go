a4d13a63b958088d0e2fe71bc549c73cc653d331
zhuliting <zhuliting@foxmail.com>
2019-05-15 11:40:04 +0800

    fix typo

--- x/tcp/msgs.go
+++ x/tcp/msgs.go
@@ -5,11 +5,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-
 // MsgTransfer defines a transfer message
 type MsgTransfer struct {
-	From sdk.AccAddress
-	To sdk.AccAddress
+	From  sdk.AccAddress
+	To    sdk.AccAddress
 	Value sdk.Coin
 	// State []byte // TODO
 	// Fee sdk.Coin
@@ -20,37 +19,34 @@ type MsgTransfer struct {
 	// To balance + Value = NewState of To
 }
 
-
 // MsgContractDeploy defines a ContractDeploy message
 type MsgContractDeploy struct {
-	From sdk.AccAddress
-	CID sdk.AccAddress
-	Code []byte
+	From     sdk.AccAddress
+	CID      sdk.AccAddress
+	Code     []byte
 	CodeHash string
-	State []byte // TODO
-	Fee sdk.Coin
+	State    []byte // TODO
+	Fee      sdk.Coin
 }
 
 // MsgContractExec defines a ontractExec message
 type MsgContractExec struct {
-	From sdk.AccAddress
-	CID sdk.AccAddress
-	State []byte // TODO
+	From          sdk.AccAddress
+	CID           sdk.AccAddress
+	State         []byte         // TODO
 	RequestParams []RequestParam // TODO
-	Proof []byte // TODO
-	ResultHash string
-	Fee sdk.Coin
+	Proof         []byte         // TODO
+	ResultHash    string
+	Fee           sdk.Coin
 }
-
-
 
 // NewMsgTransfer is a constructor function for MsgTransfer
 func NewMsgTransfer(from sdk.AccAddress, to sdk.AccAddress, value sdk.Coin, fee sdk.Coin) MsgTransfer {
 
 	return MsgTransfer{
-		From :  from,
-		To : to,
-		Value : value,
+		From:  from,
+		To:    to,
+		Value: value,
 	}
 }
 
@@ -61,7 +57,7 @@ func (msg MsgTransfer) Route() string { return "tcp" }
 func (msg MsgTransfer) Type() string { return "transfer" }
 
 // ValidateBasic runs stateless checks on the message
-func (msg MsgTransfer) ValidateBasic() sdk.Error {}
+func (msg MsgTransfer) ValidateBasic() sdk.Error {
 	if msg.From.Empty() {
 		return sdk.ErrInvalidAddress(msg.From.String())
 	}
@@ -90,7 +86,6 @@ func (msg MsgTransfer) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.From}
 }
 
-
 // NewMsgContractDeploy is a constructor function for MsgTransfer
 func NewMsgContractDeploy(from sdk.AccAddress) MsgContractDeploy {
 	// TODO
@@ -111,8 +106,6 @@ func (msg MsgContractDeploy) ValidateBasic() sdk.Error {
 
 	return nil
 }
-
-
 
 // NewMsgContractDeploy is a constructor function for MsgTransfer
 func NewMsgContractExec(from sdk.AccAddress) MsgContractExec {
