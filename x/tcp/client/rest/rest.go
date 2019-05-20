5a064a825641813ab7e3251aa66d2067baec0128
zhuliting <zhuliting@foxmail.com>
2019-05-20 15:03:27 +0800

    updated transfer rest api

--- x/tcp/client/rest/rest.go
+++ x/tcp/client/rest/rest.go
@@ -25,20 +25,55 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, 
 
 
 type transferReq struct {
-	BaseReq rest.BaseReq `json:"base_req"`
-	Name    string       `json:"name"`
-	Amount  string       `json:"amount"`
+	BaseReq rest.BaseReq	`json:"base_req"`
+	From    string			`json:"from"`
+	To 		string			`json:"to"`
+	Amount  string			`json:"amount"`
 }
 
 // transferHandler
 func transferHandler(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO
-
 		var req transferReq
-		var msg tcp.MsgTransfer
+
+		if !rest.ReadRESTReq(w, r, cdc, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
 
 		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+		// parse from address
+		addrFrom, err := sdk.AccAddressFromBech32(req.From)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// parse to address
+		addrTo, err := sdk.AccAddressFromBech32(req.To)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// parse amount
+		coins, err := sdk.ParseCoins(req.Amount)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		// create the message
+		msg := tcp.NewMsgTransfer(addrFrom, addrTo, coins)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
 		clientrest.WriteGenerateStdTxResponse(w, cdc, cliCtx, baseReq, []sdk.Msg{msg})
 	}
 }
